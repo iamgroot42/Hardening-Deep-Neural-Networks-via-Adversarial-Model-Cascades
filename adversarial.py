118133743cb93f5b10b550380e474fdb626ce5f1
Anshuman Suri <anshuman14021@iiitd.ac.in>
2016-12-18 00:43:03 +0530

    FGSM working; bug in adversarial example testing, same accuracies. Need to look into that.

--- adversarial.py
+++ adversarial.py
@@ -21,7 +21,7 @@ FLAGS = flags.FLAGS
 
 flags.DEFINE_string('train_dir', '/tmp', 'Directory storing the saved model.')
 flags.DEFINE_string('filename', 'mnist.ckpt', 'Filename to save model under.')
-flags.DEFINE_integer('nb_epochs', 6, 'Number of epochs to train model')
+flags.DEFINE_integer('nb_epochs', 1, 'Number of epochs to train model')
 flags.DEFINE_integer('batch_size', 128, 'Size of training batches')
 flags.DEFINE_float('learning_rate', 0.1, 'Learning rate for training')
 
@@ -29,74 +29,70 @@ flags.DEFINE_float('learning_rate', 0.1, 'Learning rate for training')
 def main(argv=None):
 	# Set TF random seed to improve reproducibility
 	tf.set_random_seed(1234)
-
 	# Image dimensions ordering should follow the Theano convention
 	if keras.backend.image_dim_ordering() != 'th':
 		keras.backend.set_image_dim_ordering('th')
-		print("INFO: '~/.keras/keras.json' sets 'image_dim_ordering' to 'tf', temporarily setting to 'th'")
-
 	# Create TF session and set as Keras backend session
 	sess = tf.Session()
 	keras.backend.set_session(sess)
-	print("Created TensorFlow session and set Keras backend.")
-
 	# Get MNIST test data
 	X_train, Y_train, X_test, Y_test = data_mnist()
-	print("Loaded MNIST test data.")
-
 	assert Y_train.shape[1] == 10.
 	label_smooth = .1
 	Y_train = Y_train.clip(label_smooth / 9., 1. - label_smooth)
 
-	# Define input TF placeholder
+	# Define input TF placeholder (for blackbox model)
 	x = tf.placeholder(tf.float32, shape=(None, 1, 28, 28))
 	y = tf.placeholder(tf.float32, shape=(None, 10))
 
-	# Define TF model graph
+	# Define TF model graph (for blackbox model)
 	model = model_mnist()
 	predictions = model(x)
-	print("Defined TensorFlow model graph.")
 
 	def evaluate():
 		# Evaluate the accuracy of the MNIST model on legitimate test examples
 		accuracy = tf_model_eval(sess, x, y, predictions, X_test, Y_test)
-		assert X_test.shape[0] == 10000, X_test.shape
 		print('Test accuracy on legitimate test examples: ' + str(accuracy))
 
-	# Train an MNIST model
+	# print("Jacobian: ")
+	# J = tf.test.compute_gradient(x, (None, 1, 28, 28), Y_test, Y_test.shape)
+	# print(J)	
+
+	# ToDo: Replace training data for proxy with Jacobian Augmented Data
+	X_train_p = X_train[:100,:,:,:]
+	Y_train_p = Y_train[:100:]
+	X_train = X_train[:500,:,:,:]
+	Y_train = Y_train[:500:]
+	X_test = X_test[:200]
+	Y_test = Y_test[:200]
+
+	# Train an MNIST model (blackbox model)
 	tf_model_train(sess, x, y, predictions, X_train, Y_train, evaluate=evaluate)
 
-	# Craft adversarial examples using Fast Gradient Sign Method (FGSM)
-	adv_x = fgsm(x, predictions, eps=0.3)
-	X_test_adv, = batch_eval(sess, [x], [adv_x], [X_test])
-	assert X_test_adv.shape[0] == 10000, X_test_adv.shape
+	# Define TF model graph (for proxy model)
+	model_p = model_mnist()
+	predictions_p = model(x)
 
+	# Train an MNIST model (proxy model)
+	tf_model_train(sess, x, y, predictions_p, X_train_p, Y_train_p, evaluate=evaluate)
+
+	# Craft adversarial examples using Fast Gradient Sign Method (FGSM)
+	adv_x = fgsm.fgsm(x, predictions, eps=0.3)
+	X_test_adv, = batch_eval(sess, [x], [adv_x], [X_test])
 	# Evaluate the accuracy of the MNIST model on adversarial examples
 	accuracy = tf_model_eval(sess, x, y, predictions, X_test_adv, Y_test)
-	print('Test accuracy on adversarial examples: ' + str(accuracy))
+	print('Misclassification accuracy on adversarial examples (black box): ' + str(1.0 - accuracy))
 
-	print("Repeating the process, using adversarial training")
-	# Redefine TF model graph
-	model_2 = model_mnist()
-	predictions_2 = model_2(x)
-	adv_x_2 = fgsm(x, predictions_2, eps=0.3)
-	predictions_2_adv = model_2(adv_x_2)
+	# Craft adversarial examples from proxy network and check their accuracy on black box
+	adv_x_p = fgsm.fgsm(x, predictions_p, eps=0.3)
+	X_test_adv_p, = batch_eval(sess, [x], [adv_x_p], [X_test])
+	# Evaluate the accuracy of the MNIST model on adversarial examples
+	accuracy_p = tf_model_eval(sess, x, y, predictions_p, X_test_adv_p, Y_test)
+	print('Misclassification accuracy on adversarial examples (proxy): ' + str(1.0 - accuracy_p))
 
-
-	def evaluate_2():
-		# Evaluate the accuracy of the adversarialy trained MNIST model on
-		# legitimate test examples
-		accuracy = tf_model_eval(sess, x, y, predictions_2, X_test, Y_test)
-		print('Test accuracy on legitimate test examples: ' + str(accuracy))
-
-		# Evaluate the accuracy of the adversarially trained MNIST model on
-		# adversarial examples
-		accuracy_adv = tf_model_eval(sess, x, y, predictions_2_adv, X_test, Y_test)
-		print('Test accuracy on adversarial examples: ' + str(accuracy_adv))
-
-	# Perform adversarial training
-	tf_model_train(sess, x, y, predictions_2, X_train, Y_train, predictions_adv=predictions_2_adv,
-			evaluate=evaluate_2)
+	# Check classification accuracy of adversarial examples of proxy on black box
+	accuracy_bp = tf_model_eval(sess, x, y, predictions, X_test_adv_p, Y_test)
+	print('Misclassification accuracy_bp on adversarial examples on blackbox from proxy: ' + str(1.0 - accuracy_bp))
 
 
 if __name__ == '__main__':
