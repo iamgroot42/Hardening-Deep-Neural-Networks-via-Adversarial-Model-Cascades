027fc691cb052d6cc35e0c37ac5ddc3156298f06
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-01-07 16:14:57 +0530

    Fixed peipeline; Adding outputs.

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
 # Train blackbox model
-python train_model.py --is_blackbox True --save_here BM > OUTPUT_LOG
+python train_model.py --is_blackbox True --save_here BM
 # Train proxy model
-python train_model.py --is_blackbox False --save_here PM >> OUTPUT_LOG
+python train_model.py --is_blackbox False --save_here PM
 # Generate adversarial examples for proxy model
-python generate_adversarials.py --model_path PM --adversary_path_x ADX --adversary_path_y ADY --fgsm_eps 0.1 >> OUTPUT_LOG
+python generate_adversarials.py --model_path PM --adversary_path_x ADX --adversary_path_y ADY --fgsm_eps 0.8
 # Test misclassification accuracy of proxy adversarial examples on blackbox model
-python cross_test.py --model_path BM --adversary_path_x ADX.npy --adversary_path_y ADY.npy >> OUTPUT_LOG
+python cross_test.py --model_path BM --adversary_path_x ADX.npy --adversary_path_y ADY.npy
+
