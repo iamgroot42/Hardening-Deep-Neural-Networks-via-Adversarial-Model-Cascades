b1a32914ee6cc5894b3de7005cab58254b0ca1af
Anshuman Suri <anshuman14021@iiitd.ac.in>
2017-01-07 16:09:42 +0530

    Visualize images

--- pipeline.sh
+++ pipeline.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Train blackbox model
-python train_model.py --is_blackbox True --save_here BM
+python train_model.py --is_blackbox True --save_here BM > OUTPUT_LOG
 # Train proxy model
-python train_model.py --is_blackbox False --save_here PM
+python train_model.py --is_blackbox False --save_here PM >> OUTPUT_LOG
 # Generate adversarial examples for proxy model
-python generate_adversarials.py --model_path PM --adversary_path_x ADX --adversary_path_y ADY
+python generate_adversarials.py --model_path PM --adversary_path_x ADX --adversary_path_y ADY --fgsm_eps 0.1 >> OUTPUT_LOG
 # Test misclassification accuracy of proxy adversarial examples on blackbox model
-python cross_test.py --model_path BM --adversary_path_x ADX.npy --adversary_path_y ADY.npy
+python cross_test.py --model_path BM --adversary_path_x ADX.npy --adversary_path_y ADY.npy >> OUTPUT_LOG
---
+++ visualize_adex.py
@@ -0,0 +1,37 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import numpy as np
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+from tensorflow.python.platform import app
+from tensorflow.python.platform import flags
+
+
+FLAGS = flags.FLAGS
+
+flags.DEFINE_string('adversary_path_x', 'adversaries_x.npy', 'Path where adversarial examples are to be saved')
+flags.DEFINE_string('adversary_path_y', 'adversaries_y.npy', 'Path where adversarial labels are to be saved')
+flags.DEFINE_integer('example_index', 0, 'Which index do you want to visualize?')
+
+
+def main(argv=None):
+
+	X_test_adv = np.load(FLAGS.adversary_path_x)
+	Y_test = np.load(FLAGS.adversary_path_y)
+
+	X_test_adv = X_test_adv[FLAGS.example_index]
+	Y_test = Y_test[FLAGS.example_index]
+
+	X_test_adv = X_test_adv.reshape((28, 28))
+	plt.matshow(X_test_adv,  cmap='gray')
+	plt.savefig('adv_example.png')
+
+
+if __name__ == '__main__':
+	app.run()
